23e8bbc9b7c90a7f3fd041a39189a47c3ad0f71e
aditya-sengupta <aditya.sengupta@berkeley.edu>
2021-11-15 21:00:48 +0000

    getting used to python calls

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # soar
-SEAL Optical Adaptations with Rust: an attempt at doing real-time control on the UC Santa Cruz SEAL testbed in a language that's more suited to it, but that still has Python interoperability.
+SEAL Odaptive Aptics with Rust: an attempt at doing real-time control on the UC Santa Cruz SEAL testbed in a language that's more suited to it, but that still has Python interoperability.
+
+This project uses PyO3 and `maturin` to allow Rust functions to be called from Python. To access 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use pyo3::types::PyDict;
+use pyo3::types::IntoPyDict;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn cwd() -> PyResult<String> {
+    Python::with_gil(|py| {
+        let locals = [("os", py.import("os")?)].into_py_dict(py);
+        let code = "os.getcwd()";
+        let cwd: String = py.eval(code, None, Some(&locals))?.extract()?;
+
+        Ok(format!("Python says we are in {}", cwd))
+    })
 }
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -12,6 +26,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn soar(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
+    m.add_function(wrap_pyfunction!(cwd, m)?)?;
     Ok(())
 }
