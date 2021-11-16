b8fcc03398a235c17f114d45e3bdd706a49d2ae3
aditya-sengupta <aditya.sengupta@berkeley.edu>
2021-11-16 00:13:52 +0000

    function calling works now

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
+extern crate ndarray;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::types::PyDict;
 use pyo3::types::IntoPyDict;
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+use numpy::{PyArray1,PyArray2};
 
 #[pyfunction]
 fn cwd() -> PyResult<String> {
@@ -20,12 +16,27 @@ fn cwd() -> PyResult<String> {
     })
 }
 
+// Take in the name of an AO loop and call all the functions we'd need to call while closing the loop
+#[pyfunction]
+fn call_aoloop(name: String) -> PyResult<()> {
+    Python::with_gil(|py| {
+        println!("Trying to import {}", name);
+        let aoloop = PyModule::import(py, &name)?;
+        let img: &PyArray2<f64> = aoloop.getattr("getim")?.call1((0.001,))?.downcast::<PyArray2<f64>>()?;
+        let zcoeffs: &PyArray1<f64> = aoloop.getattr("measure")?.call1((img,))?.downcast::<PyArray1<f64>>()?;
+        let u: &PyArray1<f64> = aoloop.getattr("control")?.call1((zcoeffs,))?.downcast::<PyArray1<f64>>()?;
+        let dmc: &PyArray2<f64> = aoloop.getattr("control_to_dmc")?.call1((u,))?.downcast::<PyArray2<f64>>()?;
+        let _dmcaf: &PyArray2<f64> = aoloop.getattr("applydmc")?.call1((dmc,))?.downcast::<PyArray2<f64>>()?;
+        Ok(())
+    })
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn soar(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(cwd, m)?)?;
+    m.add_function(wrap_pyfunction!(call_aoloop, m)?)?;
     Ok(())
 }
